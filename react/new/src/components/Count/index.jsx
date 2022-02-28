300dad87525b3c29150f3fca0e56d8b81724d333
pw <1040479402@qq.com>
2022-02-28 13:19:37 +0800

    20220228

--- react/new/src/components/Count/index.jsx
+++ react/new/src/components/Count/index.jsx
@@ -1,44 +1,29 @@
 import React, { Component } from 'react'
-import store from '../../redux/store'
-import {createIncrementAction,createDecrementAction,createIncrementAsyncAction} from '../../redux/count_action'
 
 export default class Count extends Component {
-    /* componentDidMount() {
-        //检测redux中状态的变化，只要变化，就调用render
-        store.subscribe(() => {
-            this.setState({})
-        })
-    } */
-
     //加法
     increment = () => {
         const { value } = this.selectNumber
-        store.dispatch(createIncrementAction(value*1))
+
     }
     //减法
     decremnet = () => {
         const { value } = this.selectNumber
-        store.dispatch(createDecrementAction(value*1))
+
     }
     //奇数再加
     incrementIfOdd = () => {
         const { value } = this.selectNumber
-        const count = store.getState()
-        if (count % 2 !== 0) {
-            store.dispatch(createIncrementAction(value*1))
-        }
+
     }
     //异步加
     incrementAsync = () => {
         const { value } = this.selectNumber
-        // setTimeout(() => {
-            store.dispatch(createIncrementAsyncAction(value*1,500))
-        // }, 500)
     }
     render() {
         return (
             <div>
-                <h1>当前求和为：{store.getState()}</h1>
+                <h1>当前求和为：{}</h1>
                 <select ref={c => this.selectNumber = c}>
                     <option value="1">1</option>
                     <option value="2">2</option>
