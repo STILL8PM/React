874a141277dc946f33bd825e44ffb0ed1a369bc8
pw <1040479402@qq.com>
2022-02-28 15:20:55 +0800

    20220228

--- react/new/src/components/Count/index.jsx
+++ react/new/src/components/Count/index.jsx
@@ -4,26 +4,29 @@ export default class Count extends Component {
     //加法
     increment = () => {
         const { value } = this.selectNumber
-
+        this.props.jia(value * 1)
     }
     //减法
     decremnet = () => {
         const { value } = this.selectNumber
-
+        this.props.jian(value * 1)
     }
     //奇数再加
     incrementIfOdd = () => {
         const { value } = this.selectNumber
-
+        if (this.props.count % 2 !== 0) {
+            this.props.jia(value * 1)
+        }
     }
     //异步加
     incrementAsync = () => {
         const { value } = this.selectNumber
+        this.props.jiaAsync(value*1,500)
     }
     render() {
         return (
             <div>
-                <h1>当前求和为：{}</h1>
+                <h1>当前求和为:{this.props.count}</h1>
                 <select ref={c => this.selectNumber = c}>
                     <option value="1">1</option>
                     <option value="2">2</option>
--- react/new/src/containers/Count/index.jsx
+++ react/new/src/containers/Count/index.jsx
@@ -1,1 +1,35 @@
-import {} from 'react-redux'
+//引入Count的ui组件
+import CountUI from '../../components/Count'
+//引入action
+import { createIncrementAction, createDecrementAction, createIncrementAsyncAction } from '../../redux/count_action'
+
+//引入connect用于连接UI组件与redux
+import { connect } from 'react-redux'
+
+
+/* 
+    mapStateToProps函数返回的是一个对象
+    返回的对象中的key就作为传递给UI组件props的key，value就作为传给给UI组件props的value
+    mapStateToProps用于传递状态
+ */
+function mapStateToProps(state) {
+    return { count: state }
+}
+
+
+/* 
+    mapDispatchToProps函数返回的是一个对象
+    返回的对象中的key就作为传递给UI组件props的key，value就作为传给给UI组件props的value
+    mapDispatchToProps用于传递操作状态的方法
+ */
+function mapDispatchToProps(dispatch) {
+    return {
+        //通知redux执行加法
+        jia: (number) => dispatch(createIncrementAction(number)),
+        jian: (number) => dispatch(createDecrementAction(number)),
+        jiaAsync: (number,time) => dispatch(createIncrementAsyncAction(number,time)),
+    }
+}
+
+//创建并暴露一个Count的容器组件
+export default connect(mapStateToProps, mapDispatchToProps)(CountUI)
