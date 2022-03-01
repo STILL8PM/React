7d3b8a5e14fecb94c7e50a76d0275ce7e72ad5b3
pw <1040479402@qq.com>
2022-03-01 11:43:43 +0800

    20220301

---
+++ react/react-extension/src/components/1_setState/index.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class Demo extends Component {
+    state = { count: 0 }
+
+    add = () => {
+        //对象式的setState
+        /* //获取原来的count值
+        const  {count} = this.state
+        //更新状态
+        this.setState({count:count+1},()=> {
+            console.log(this.state.count);
+        })
+        console.log(this.state);
+         */
+
+        //函数式的setState
+        /* this.setState((state, props) => {
+            console.log(state, props);
+            return { count: state.count + 1 }
+        }) */
+        //精简--->
+        this.setState(state=> ({count:state.count+1}))
+    }
+    render() {
+        return (
+            <div>
+                <h1>当前求和为:{this.state.count}</h1>
+                <button onClick={this.add}>点我+1</button>
+            </div>
+        )
+    }
+}
