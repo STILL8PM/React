e01a0c7af457b5d8af0083ce12686d23dcb4187a
pw <1040479402@qq.com>
2022-03-01 19:20:33 +0800

    20220301

---
+++ react/react-extension/src/components/7_renderProps/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class Parent extends Component {
+    render() {
+        return (
+            <div>我是Parent组件</div>
+        )
+    }
+}
