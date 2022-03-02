f30aab9e80ea2d8f1d54167b942536335eb5020d
pw <1040479402@qq.com>
2022-03-02 12:11:29 +0800

    20220302

--- react/react-extension/src/components/7_renderProps/index.jsx
+++ react/react-extension/src/components/7_renderProps/index.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
 import './index.css'
+import C from '../1_setState/index'
 
 export default class Parent extends Component {
     render() {
         return (
-            <div>我是Parent组件</div>
+            <div className='parent'>
+                <h3>我是Parent组件</h3>
+                <A render={(name) => <C name={name}/>} />
+            </div>
+        )
+    }
+}
+
+class A extends Component {
+    state = { name: 'Tom' }
+    render() {
+        console.log(this.props)
+        const { name } = this.state
+        return (
+            <div className='a'>
+                <h3>A组件</h3>
+                {this.props.render(name)}
+            </div>
+        )
+    }
+}
+class B extends Component {
+    render() {
+        console.log('B---render');
+        return (
+            <div className='b'>
+                <h3>B组件,{this.props.name}</h3>
+            </div>
         )
     }
 }
